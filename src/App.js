51f9ae02f3fd54c3fb3aa559bd82ebed8151af5a
Медведев Иван <ranzar2000@gmail.com>
2023-08-02 22:22:46 +0300

    add sliders
    lift and right side sliders: image title subtitle with onscroll animation

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import logo from './logo.svg';
 import './App.css';
 import pic_1 from "./assets/pic-1.jpg";
+import pic_2 from "./assets/pic-2.jpg";
+import pic_3 from "./assets/pic-3.jpg";
 import Hero from "./components/Hero";
+import Slider from "./components/Slider";
 
 function App() {
   return (
@@ -21,6 +24,8 @@ function App() {
         </a>
       </header>
       <div id="hero"><Hero imageSrc={pic_1} /></div>
+      <div id="slider1"><Slider imageSrc={pic_2} title={"Be an explorer!"} subtitle={"Travel around the World just for fun."}/></div>
+      <div id="slider2"><Slider imageSrc={pic_3} title={"Memories for a lifetime"} subtitle={"Your dream vocation is only a few clicks away."} flipped={true} /></div>
     </div>
   );
 }
